be24e8eb94e29758837bf29e824c9fd66489d84c
Tomasz Piotrowski <TPiotrowski@cardlytics.com>
2022-03-02 20:14:27 +0000

    updated schema and mapping for Bonsai and Holder entities

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,53 +1,98 @@
 import {
-  Approval as ApprovalEvent,
-  ApprovalForAll as ApprovalForAllEvent,
-  OwnershipTransferred as OwnershipTransferredEvent,
   Transfer as TransferEvent
 } from "../generated/Token/Token"
 import {
-  Approval,
-  ApprovalForAll,
-  OwnershipTransferred,
-  Transfer
+  Bonsai,
+  Holder
 } from "../generated/schema"
+import {
+  log,
+  ipfs,
+  json,
+  JSONValue
+} from "@graphprotocol/graph-ts"
 
-export function handleApproval(event: ApprovalEvent): void {
-  let entity = new Approval(
-    event.transaction.hash.toHex() + "-" + event.logIndex.toString()
-  )
-  entity.owner = event.params.owner
-  entity.approved = event.params.approved
-  entity.tokenId = event.params.tokenId
-  entity.save()
-}
-
-export function handleApprovalForAll(event: ApprovalForAllEvent): void {
-  let entity = new ApprovalForAll(
-    event.transaction.hash.toHex() + "-" + event.logIndex.toString()
-  )
-  entity.owner = event.params.owner
-  entity.operator = event.params.operator
-  entity.approved = event.params.approved
-  entity.save()
-}
-
-export function handleOwnershipTransferred(
-  event: OwnershipTransferredEvent
-): void {
-  let entity = new OwnershipTransferred(
-    event.transaction.hash.toHex() + "-" + event.logIndex.toString()
-  )
-  entity.previousOwner = event.params.previousOwner
-  entity.newOwner = event.params.newOwner
-  entity.save()
-}
+const ipfsHash = "QmVtJUJrjWfthrGFvkwFciZcvoNLzCrC6EXAQwimYBUZeQ"
 
 export function handleTransfer(event: TransferEvent): void {
-  let entity = new Transfer(
-    event.transaction.hash.toHex() + "-" + event.logIndex.toString()
-  )
-  entity.from = event.params.from
-  entity.to = event.params.to
-  entity.tokenId = event.params.tokenId
-  entity.save()
+  let bonsai = Bonsai.load(event.params.tokenId.toString());
+
+  if (bonsai == null) {
+    bonsai = new Bonsai(event.params.tokenId.toString());
+    bonsai.tokenID = event.params.tokenId;
+    bonsai.tokenURI = ipfsHash + "/" + event.params.tokenId.toString();
+
+    let metadata = ipfs.cat(bonsai.tokenURI);
+    if (metadata) {
+      const value = json.fromBytes(metadata).toObject();
+      if (value) {
+        const name = value.get("name");
+        if (name) {
+          bonsai.name = name.toString();
+        }
+
+        const image = value.get("image");
+        if (image) {
+          bonsai.image = image.toString();
+        }
+
+        const animation_url = value.get("animation_url");
+        if (animation_url) {
+          bonsai.animation_url = animation_url.toString();
+        }
+      }
+
+      let attributes: JSONValue[];
+      let bonsaiAttributes = value.get("attributes");
+      if (bonsaiAttributes) {
+        attributes = bonsaiAttributes.toArray();
+
+        for (let i=0; i<attributes.length; i++) {
+          let item = attributes[i].toObject();
+          let trait: string;
+          let traitName = item.get("trait_type");
+          if (traitName) {
+            trait = traitName.toString();
+            let value: string;
+            let traitValue = item.get("value");
+            if (traitValue) {
+              value = traitValue.toString();
+              if (trait == "Pot") {
+                bonsai.Pot = value;
+              }
+              if (trait == "Pet") {
+                bonsai.Pet = value;
+              }
+              if (trait == "Bark") {
+                bonsai.Bark = value;
+              }
+              if (trait == "Leaves") {
+                bonsai.Leaves = value;
+              }
+              if (trait == "Fruit") {
+                bonsai.Fruit = value;
+              }
+              if (trait == "Number of fruit") {
+                bonsai.NumberOfFruit = value;
+              }
+              if (trait == "Ground cover") {
+                bonsai.GroundCover = value;
+              }
+              if (trait == "Background") {
+                bonsai.Background = value;
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+  bonsai.holder = event.params.to.toHexString();
+  bonsai.save();
+
+  let holder = Holder.load(event.params.to.toHexString());
+  if (!holder) {
+    holder = new Holder(event.params.to.toHexString());
+    holder.save();
+  }
 }
